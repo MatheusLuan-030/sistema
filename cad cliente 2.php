c4cadfe5d9b7e60e4d33134c69449c1909809b8c
MatheusLuan-030 <142815120+MatheusLuan-030@users.noreply.github.com>
2023-10-24 11:46:06 -0300

    Adicionar conteudo

--- cad cliente 2.php
+++ cad cliente 2.php
@@ -24,3 +24,35 @@
         <h2 class="subtitulo"> Data de nascimento:</h2>
         <input type="date" name="data_nas_cli">
 </td>
+</td>
+</tr>
+<tr>
+<tr>
+<td>
+    <h2 class="subtitulo"> Bairro:</h2>
+    <input type="text" name="bairro_cli" size="60">
+</td>
+</tr>
+<td>
+    <h2 class="subtitulo"> Rua:</h2>
+    <input type="text" name="rua_cli" id="moeda" size=60>
+</td>
+</tr>
+<tr>
+    <h2 class="subtitulo"> Número:</h2>
+    <input type="text" name="num_cli" size="20">
+</td>
+</tr>
+
+
+
+<tr>
+<td><h2 class="subtitulo" style="display:inline;"> Estado:</h2>
+<select name="estado_cli"
+<?php  
+        require('conexao.php')
+        $sql="SELECT est_cod, est_nome FROM estado"
+        $result=mysqli_query($db,$sql)or die(mysqli_error());
+        echo"<option value="selecione...'>Selecione...</option>";
+        while($dados=mysqli_fetch_array($result))(
+        echo"<option value='".utf8=encode($dados['est_cod'])."'>".utf8_encode($dados
